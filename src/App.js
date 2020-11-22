91ef0ba8e56b6362ef7f83a6709d3271cfe1a9e6
Faruq Bakare <bakarefaruq.a@gmail.com>
2020-11-22 19:33:01 +0100

    add and styling header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import logo from './Group1.png';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Header from './Header';
 
 function App() {
   return (
-    <div className="app">
-      <h1>Hello</h1>
-      <img src={logo} className="App-logo" alt="logo" />
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>login page</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <h1>Home page!</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
