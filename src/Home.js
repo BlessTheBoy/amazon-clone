72cc26f93de9982e48f054e92dfd0f58f7836b09
Faruq Bakare <bakarefaruq.a@gmail.com>
2020-11-23 13:08:45 +0100

    product row design

--- src/Home.js
+++ src/Home.js
@@ -7,17 +7,26 @@ function Home() {
     <div className="home">
       <img
         className="home__image"
-        src="https://images-eu.ssl-images-amazon/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB.CB428684220_.jpg"
+        src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
         alt=""
       />
 
-      <Product
-        id="123356"
-        title="The Lean Startup: How Constant Innovation Creates"
-        price={11.96}
-        rating={5}
-        image="https://images-eu.ssl-images-amazon/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
-      />
+      <div className="home__row">
+        <Product
+          id="123356"
+          title="The Lean Startup: How Constant Innovation Creates Radically Succesfull Bussiness Paperback"
+          price={11.96}
+          rating={5}
+          image="https://images-eu.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+        />
+        <Product
+          id="123356"
+          title="The Lean Startup: How Constant Innovation Creates Radically Succesfull Bussiness Paperback"
+          price={11.96}
+          rating={5}
+          image="https://images-eu.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+        />
+      </div>
     </div>
   );
 }
--- src/Product.js
+++ src/Product.js
@@ -1,21 +1,29 @@
 import React from "react";
 import "./Product.css";
+import StarIcon from "@material-ui/icons/Star";
 
 function Product({ id, title, image, price, rating }) {
   return (
     <div className="product">
-      <p>{title}</p>
-      <p className="product__price">
-        <small>$</small>
-        <strong>{price}</strong>
-      </p>
-      <div className="product__rating">
-        {Array(rating)
-          .fill()
-          .map(() => (
-            <p>*</p>
-          ))}
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map(() => (
+              <p>
+                <StarIcon style={{ color: "#d4af37" }} />
+              </p>
+            ))}
+        </div>
       </div>
+
+      <img src={image} alt="" />
+      <button>Add to basket</button>
     </div>
   );
 }
